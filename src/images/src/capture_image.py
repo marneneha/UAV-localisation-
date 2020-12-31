ab4b64cc0915729b2e84bddb303fc97e907bdb51
nehamarne27 <nehamarne27gmail,com>
2020-12-31 13:53:23 +0530

    o/p from video if fine

--- src/images/src/capture_image.py
+++ src/images/src/capture_image.py
@@ -7,24 +7,16 @@ from cv_bridge import CvBridge
 import sys
 
 bridge = CvBridge()
-
-def image_callback(ros_image):
-    print("i am getting in")
-    global bridge
-    cv_image = bridge.imgmsg_to_cv2(ros_image, "bgr8")
-    print("line 2")
-    cv2.imshow("Drone Camera Feed", cv_image)
-    cv2.waitKey(3)
-    print("i m in the end")
+cap = cv2.VideoCapture(0)
 
   
 def main(args):
     rospy.init_node('image_converter', anonymous=True)
-    print("i m in")
     try:
 	while(1):
-		print("i got in")
-        	image1= rospy.Subscriber("/uav1/bluefox_optflow/image_raw",Image,image_callback) 
+		_,frame = cap.read()
+		cv2.imshow('Contours',frame)              
+		k=cv2.waitKey(10)
     except KeyboardInterrupt:
         print("Shutting Down")
     cv2.destroyAllWindows()
