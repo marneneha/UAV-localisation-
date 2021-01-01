26994604880d6568b668cb535598caf8a14c9e18
nehamarne27 <nehamarne27gmail,com>
2021-01-01 14:04:49 +0530

    able to get feed properly

--- src/images/src/capture_image.py
+++ src/images/src/capture_image.py
@@ -7,18 +7,27 @@ from cv_bridge import CvBridge
 import sys
 
 bridge = CvBridge()
-cap = cv2.VideoCapture(0)
+
+def image_callback(ros_image):
+    print("i am getting in")
+    while(1):
+	    print("in while")
+	    global bridge
+	    cv_image = bridge.imgmsg_to_cv2(ros_image, "bgr8")
+	    cv2.imshow("Feed", cv_image)
+	    cv2.waitKey()
+	    print("end")
+
 
   
 def main(args):
     rospy.init_node('image_converter', anonymous=True)
     try:
 	while(1):
-		_,frame = cap.read()
-		cv2.imshow('Contours',frame)              
-		k=cv2.waitKey(10)
+        	image1= rospy.Subscriber("/uav1/bluefox_optflow/image_raw",Image,image_callback) 
+		rospy.spin()
     except KeyboardInterrupt:
         print("Shutting Down")
-    cv2.destroyAllWindows()
+    	cv2.destroyAllWindows()
 if __name__ == '__main__':
     main(sys.argv)
